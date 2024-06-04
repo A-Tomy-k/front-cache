417bea824afed2964eeba060a79573e2536b96db
avaqueroblanco <avakero@gmail.com>
2024-06-05 00:24:00 +0200

    debug and environment variables

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/rabbitmq/amqp091-go v1.10.0
 )
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"database/sql"
+	//"database/sql"
 	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"strings"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/patrickmn/go-cache"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
@@ -20,167 +22,177 @@ const fail_limit_value int = 1
 const rate_limit_value int = 1000
 const limit_expiration time.Duration = time.Minute
 
-var api_key_cache      = cache.New(5*time.Minute, 10*time.Minute)
+var api_key_cache = cache.New(5*time.Minute, 10*time.Minute)
 var request_fail_cache = cache.New(5*time.Minute, 10*time.Minute)
-var rate_limit_cache   = cache.New(5*time.Minute, 10*time.Minute)
+var rate_limit_cache = cache.New(5*time.Minute, 10*time.Minute)
 
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	router := http.NewServeMux()
 	router.HandleFunc("POST /", incomingRequestHandler)
 
-    err := http.ListenAndServe(":8080", router)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println("Server running on :8080")
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Server running on :8080")
 }
 
-func incomingRequestHandler(w http.ResponseWriter, r *http.Request){
+func incomingRequestHandler(w http.ResponseWriter, r *http.Request) {
 
-    origin_ip, err := get_origin_ip()
-    if err != nil {
-        w.WriteHeader(http.StatusPreconditionFailed)
-        fmt.Println(err)
-        io.WriteString(w, "Unable to check origin IP")
-        return
-    }
+	origin_ip, err := get_origin_ip(r)
+	if err != nil {
+		w.WriteHeader(http.StatusPreconditionFailed)
+		fmt.Println(err)
+		io.WriteString(w, "Unable to check origin IP")
+		return
+	}
 
-    if apply_fail_limit(origin_ip) {
-        fmt.Println("Fail limit applied to IP: %s", origin_ip)
-        w.WriteHeader(http.StatusTooManyRequests)
+	if apply_fail_limit(origin_ip) {
+		fmt.Printf("Fail limit applied to IP: %s\n", origin_ip)
+		w.WriteHeader(http.StatusTooManyRequests)
 		io.WriteString(w, "Too many failed requests in less than a minute")
-        return
-    }
+		return
+	}
 
-    if apply_rate_limit(origin_ip) {
-        fmt.Println("Rate limit applied to IP: %s", origin_ip)
-        w.WriteHeader(http.StatusTooManyRequests)
+	if apply_rate_limit(origin_ip) {
+		fmt.Printf("Rate limit applied to IP: %s\n", origin_ip)
+		w.WriteHeader(http.StatusTooManyRequests)
 		io.WriteString(w, "Too many requests in less than a minute")
-        return
-    }
+		return
+	}
 
-    err = r.ParseForm()
-    if err != nil {
-        register_request_fail(origin_ip)
-        fmt.Println("Error parsing form:", err)
-        w.WriteHeader(http.StatusBadRequest)
+	err = r.ParseForm()
+	if err != nil {
+		register_request_fail(origin_ip)
+		fmt.Println("Error parsing form: ", err)
+		w.WriteHeader(http.StatusBadRequest)
 		io.WriteString(w, err.Error())
-        return
-    }
+		return
+	}
 
-    api_key := r.Form.Get("ApiKey")
-    if api_key == "" {
-        register_request_fail(origin_ip)
-        err = errors.New("ApiKey not specified")
-        fmt.Println(err.Error())
-        w.WriteHeader(http.StatusBadRequest)
+	api_key := r.Form.Get("ApiKey")
+	if api_key == "" {
+		register_request_fail(origin_ip)
+		err = errors.New("ApiKey not specified")
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusBadRequest)
 		io.WriteString(w, err.Error())
-        return
-    }
+		return
+	}
 
-    id_platform, err := get_platform(api_key)
-    if err == ApiKeyNotFound {
-        register_request_fail(origin_ip)
-        err = errors.New("ApiKey not found")
-        fmt.Println(err.Error())
-        w.WriteHeader(http.StatusUnauthorized)
+	id_platform, err := get_platform(api_key)
+	if err == ApiKeyNotFound {
+		register_request_fail(origin_ip)
+		err = errors.New("ApiKey not found")
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusUnauthorized)
 		io.WriteString(w, err.Error())
-        return
-    }
-    if err != nil {
-        fmt.Println(err.Error())
-        w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err != nil {
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
 		io.WriteString(w, "An error has occurred")
-        return
-    }
+		return
+	}
 
-    // Request Ok, send to broker to process
-    publish_message(message)
+	// Request Ok, send to broker to process
+	fmt.Printf("Request queued for platform %s\n", id_platform)
+	publish_message([]byte{})
 }
 
-func get_platform(api_key string) (string, error){
+func get_platform(api_key string) (string, error) {
 
-    // Search first in cache
-    if platform, found := api_key_cache.Get(api_key); found {
+	// Search first in cache
+	if platform, found := api_key_cache.Get(api_key); found {
+		fmt.Println("Cache hit")
 		return platform.(string), nil
 	}
 
-    // If not in cache, retrieve from database
-    var id_platform string
-    row := repository.db.QueryRow("SELECT ID_PLATAFORMA FROM PLATAFORMA WHERE API_KEY = ?", api_key)
-    if err := row.Scan(id_platform); err != nil {
-        if err == sql.ErrNoRows {
-            fmt.Println("Get plaform by Api Key [%d]: no platform found", api_key)
-            return "", ApiKeyNotFound
-        }
-        return "", fmt.Errorf("Get platform by Api Key [%d] from DB : %v", api_key, err)
-    }
-    
-    api_key_cache.Set(api_key, id_platform, cache.NoExpiration)
+	fmt.Println("Cache miss. Searching in database")
 
-    return id_platform, nil
+	// If not in cache, retrieve from database
+	var id_platform string
+	/*
+	   row := repository.db.QueryRow("SELECT ID_PLATAFORMA FROM PLATAFORMA WHERE API_KEY = ?", api_key)
+	   if err := row.Scan(id_platform); err != nil {
+	       if err == sql.ErrNoRows {
+	           fmt.Println("Get plaform by Api Key [%d]: no platform found", api_key)
+	           return "", ApiKeyNotFound
+	       }
+	       return "", fmt.Errorf("Get platform by Api Key [%d] from DB : %v", api_key, err)
+	   }
+	*/
+	fmt.Printf("ApiKey found in database. Platform %s\n", id_platform)
+	api_key_cache.Set(api_key, id_platform, cache.NoExpiration)
+
+	return id_platform, nil
 
 }
 
 func apply_fail_limit(origin_ip string) bool {
-    // Search the ip in the cache to apply fail limit
-    if value, found := request_fail_cache.Get(origin_ip); found {
+	// Search the ip in the cache to apply fail limit
+	if value, found := request_fail_cache.Get(origin_ip); found {
 		return value.(int) >= fail_limit_value
 	}
 
-    return false
+	return false
 }
 
 func apply_rate_limit(origin_ip string) bool {
 
-    // Search the ip in the cache to apply rate limit
-    if value, found := rate_limit_cache.Get(origin_ip); found {
+	// Search the ip in the cache to apply rate limit
+	if value, found := rate_limit_cache.Get(origin_ip); found {
 		return value.(int) >= rate_limit_value
 	}
 
-    // Increase the counter only if rate limit is not applied
-    _, err := rate_limit_cache.IncrementInt(origin_ip, 1)
-    if err != nil {
-        rate_limit_cache.Set(origin_ip, 1, cache.NoExpiration)
-    }
+	// Increase the counter only if rate limit is not applied
+	_, err := rate_limit_cache.IncrementInt(origin_ip, 1)
+	if err != nil {
+		rate_limit_cache.Set(origin_ip, 1, cache.NoExpiration)
+	}
 
-     // Decrease the counter after reaching the expiration time
-     time.AfterFunc(limit_expiration, func() {
-        value, err := rate_limit_cache.DecrementInt(origin_ip, 1)
-        if value < 0 || err != nil {
-            rate_limit_cache.Set(origin_ip, 0, cache.NoExpiration)
-        }
-    })
+	// Decrease the counter after reaching the expiration time
+	time.AfterFunc(limit_expiration, func() {
+		value, err := rate_limit_cache.DecrementInt(origin_ip, 1)
+		if value < 0 || err != nil {
+			rate_limit_cache.Set(origin_ip, 0, cache.NoExpiration)
+		}
+	})
 
-    return false
+	return false
 }
 
-func register_request_fail(origin_ip string){
+func register_request_fail(origin_ip string) {
 
-    _, err := request_fail_cache.IncrementInt(origin_ip, 1)
-    if err != nil {
-        request_fail_cache.Set(origin_ip, 1, cache.NoExpiration)
-    }
+	_, err := request_fail_cache.IncrementInt(origin_ip, 1)
+	if err != nil {
+		request_fail_cache.Set(origin_ip, 1, cache.NoExpiration)
+	}
 
-    // Decrease the counter after reaching the expiration time
-    time.AfterFunc(limit_expiration, func() {
-        value, err := request_fail_cache.DecrementInt(origin_ip, 1)
-        if value < 0 || err != nil {
-            request_fail_cache.Set(origin_ip, 0, cache.NoExpiration)
-        }
-    })
+	// Decrease the counter after reaching the expiration time
+	time.AfterFunc(limit_expiration, func() {
+		value, err := request_fail_cache.DecrementInt(origin_ip, 1)
+		if value < 0 || err != nil {
+			request_fail_cache.Set(origin_ip, 0, cache.NoExpiration)
+		}
+	})
 
 }
 
-func get_origin_ip() (string, error) {
+func get_origin_ip(r *http.Request) (string, error) {
 
-    forwarderForHeader := r.Header.Get("X-Forwarded-For")
-    if forwarderForHeader == "" {
-        return "", errors.New("X-Forwarded-For value is empty")
-    }
+	forwarderForHeader := r.Header.Get("X-Forwarded-For")
+	if forwarderForHeader == "" {
+		return "", errors.New("X-Forwarded-For value is empty")
+	}
 
-    // Find the index of the first comma
+	// Find the index of the first comma
 	index := strings.Index(forwarderForHeader, ",")
 
 	// Extract the substring until the first comma
@@ -191,28 +203,36 @@ func get_origin_ip() (string, error) {
 		origin_ip = forwarderForHeader // If there is no comma, take the whole string
 	}
 
-    if !is_valid_ip_address(origin_ip) {
-        return "", fmt.Errorf("Detected IP is not valid: %s", origin_ip)
-    }
-    
-    return origin_ip, nil
+	if !is_valid_ip_address(origin_ip) {
+		return "", fmt.Errorf("Detected IP is not valid: %s", origin_ip)
+	}
+
+	return origin_ip, nil
 }
 
 func is_valid_ip_address(ip string) bool {
-    // Regular expression to match IPv4 addresses
-    pattern := `^(\d{1,3}\.){3}\d{1,3}$`
+	// Regular expression to match IPv4 addresses
+	pattern := `^(\d{1,3}\.){3}\d{1,3}$`
 
-    // Compile the regular expression
-    regex := regexp.MustCompile(pattern)
+	// Compile the regular expression
+	regex := regexp.MustCompile(pattern)
 
-    // Check if the IP address matches the pattern
-    return regex.MatchString(ip)
+	// Check if the IP address matches the pattern
+	return regex.MatchString(ip)
 }
 
-func publish_message(message []byte){
+func publish_message(message []byte) {
 
-    // RabbitMQ server connection information
-	conn, err := amqp.Dial("amqp://%s:%s@%s:%d/", rabbit_user, rabbit_pwd, rabbit_host, rabbit_port)
+	rabbit_user := os.Getenv("RABBIT_USER")
+	rabbit_pwd := os.Getenv("RABBIT_PWD")
+	rabbit_host := os.Getenv("RABBIT_HOST")
+	rabbit_port := os.Getenv("RABBIT_PORT")
+	// TODO define queue
+	rabbit_queue := "TEST_QUEUE"
+	rabbit_exchange := "TEST_EXCHANGE"
+
+	// RabbitMQ server connection information
+	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s/", rabbit_user, rabbit_pwd, rabbit_host, rabbit_port))
 	if err != nil {
 		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
 	}
@@ -228,11 +248,11 @@ func publish_message(message []byte){
 	// Declare a queue
 	q, err := ch.QueueDeclare(
 		rabbit_queue, // Queue name
-		true,   // Durable
-		false,   // Delete when unused
-		false,   // Exclusive
-		false,   // No-wait
-		nil,     // Arguments
+		true,         // Durable
+		false,        // Delete when unused
+		false,        // Exclusive
+		false,        // No-wait
+		nil,          // Arguments
 	)
 	if err != nil {
 		log.Fatalf("Failed to declare a queue: %v", err)
@@ -240,10 +260,10 @@ func publish_message(message []byte){
 
 	// Publish the message to the queue
 	err = ch.Publish(
-		rabbit_exchange,// Exchange
-		q.Name, // Routing key
-		false,  // Mandatory
-		false,  // Immediate
+		rabbit_exchange, // Exchange
+		q.Name,          // Routing key
+		false,           // Mandatory
+		false,           // Immediate
 		amqp.Publishing{
 			ContentType: "text/plain",
 			Body:        message,
